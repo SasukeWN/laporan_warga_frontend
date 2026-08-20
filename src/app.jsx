9588b25a8acccca6cab746099da21fb75f3f6500
SasukeWn <dandyghaisan6@gmail.com>
2026-08-20 19:28:27 +0700

    redirect halaman

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,29 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect } from 'preact/hooks'
+import { Router, route } from 'preact-router'
 import './app.css'
 
-import { Router } from 'preact-router'
 import AdminPage from './admin_page/AdminPage'
 import WargaPage from './warga_page/WargaPage'
 
+// Komponen helper untuk melakukan redirect otomatis
+const Redirect = ({ to }) => {
+  useEffect(() => {
+    route(to, true) // true berfungsi me-replace history browser
+  }, [to])
+  return null
+}
+
 export function App() {
-
   return (
-    <>
-      <Router>
-        <AdminPage path="/laporan_admin/admin" />
+    <Router>
+      {/* Pengunjung yang membuka domain utama (/) langsung diarahkan ke form warga */}
+      <Redirect path="/" to="/laporan_warga/input_data" />
 
-        <AdminPage path="/laporan_admin/daftar_warga" />
+      <AdminPage path="/laporan_admin/admin" />
+      <AdminPage path="/laporan_admin/daftar_warga" />
+      <AdminPage path="/laporan_admin/list" />
 
-        <AdminPage path="/laporan_admin/list" />
-
-        <WargaPage path="/laporan_warga/input_data"/>
-        
-      </Router>
-    </>
+      <WargaPage path="/laporan_warga/input_data" />
+    </Router>
   )
 }
